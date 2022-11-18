4a6232d1de0e576f24479bcefd3a4caa4befbf45
414dna <dhilidhiva@gmail.com>
2022-11-19 04:39:49 +0530

    5th day

--- media_downloader.py
+++ media_downloader.py
@@ -7,9 +7,13 @@ from telethon.utils import get_extension
 import os
 import pandas as pd
 from  sqlalchemy import create_engine
+import asyncio
+import traceback
+from utils import decode_pass
+
+config=yaml.load(open('config.yaml'),Loader=yaml.FullLoader)
 
 
-config=yaml.safe_load('config.yaml')
 
 logging.basicConfig(
     level=logging.INFO,
@@ -20,68 +24,91 @@ logging.basicConfig(
 
 log=logging.getLogger('download_log')
 
+# logging.info(config)
+
 class Media_downloader():
     
     def __init__(self) -> None:
         self.engine=create_engine("postgresql://postgres:Carrotpy123@localhost/postgres")
         self.downd_path=''
         self.client=''
+        self.api_hash_config=config['API_HASH']
+        self.api_id_config=config['API_ID']
+        self.channel_ids=config['CHANNEL_ID']
+        self.passw=decode_pass(config['PASSWORD'])
+        # logging.info(decode_pass(config['PASSWORD']))
+        self.ph =config['PHONE_NUMBER']
+        # print(self.channel_ids)
+        self.last_message_id=0
+
         
         
-        
-        
+    async def main(self):
+        self.client= await self.client_connect(self.api_id_config,self.api_hash_config)
+        if self.client:
+            await self.history_data()
+              
     async def client_connect(self,api_id:int,api_hash:str):
-        api_id=25828699
-        api_hash='eea3a6141d6966d45c7475603a8b6739'
-        client = TelegramClient('media_download5', api_id, api_hash)
-        await client.connect()
-        return await client.start(phone='+919789259032',password='Dna@414#')
+        try:
+            # api_id=25828699
+            # api_hash='eea3a6141d6966d45c7475603a8b6739'
+            client = TelegramClient('media_download', api_id, api_hash)
+            await client.connect()
+            if client.is_connected():
+                logging.info('connected')
+            return await client.start(phone=self.ph_nm,password=self.passw)
+        except:
+            logging.warning('connection failed')
+            return False
         
     async def history_data(self):
         alloweded_media_list=['.pdf','.mp4','.jpg']
+        
         try:
             async for d in  self.client.iter_messages('iamadhee', reverse=False):
                 if d.media and get_extension (d.media) in alloweded_media_list:
-                    if not  _is_exist(chat_id=str(d.chat_id),msg_id=d.id):
+                    if not  self._is_exist(chat_id=str(d.chat_id),msg_id=d.id):
                         filename=await d.download_media(file=f'{os.getcwd()}\\iamadhee')
                         if filename :
-                            d=update_audict(file_nm=filename.split('\\')[-1],chat_id=str(d.chat_id),msg_id=d.id,date=str(d.date),status='Success')
+                            d=self.update_audict(file_nm=filename.split('\\')[-1],chat_id=str(d.chat_id),msg_id=d.id,date=str(d.date),status='Success')
                         else:
-                            d=update_audict(file_nm=filename.split('\\')[-1],chat_id=d.chat_id,msg_id=d.id,date=str(d.date),status='Fail')
+                            d=self.update_audict(file_nm=filename.split('\\')[-1],chat_id=d.chat_id,msg_id=d.id,date=str(d.date),status='Fail')
                         
         except Exception as e:
-            print(e)
-        def _is_exist(self,chat_id:str,msg_id:int) -> bool:
-            
-            try:
-                data=self.engine.execute(f"select distinct message_id,channel_id from telegram_loger.download_audict where message_id ={msg_id} and channel_id='{chat_id}' and status <> 'Fail' ;").fetchall()
-                print(data)
-                if data:
-                    return True
-                else:
-                    return False
-                    
-            except Exception as e:
-                print(e)
-                return False
-                
-            finally:
-                self.engine.dispose()
-        def update_audict(self,chat_id:str,msg_id:int,date:str,file_nm:str,status:str) -> bool:
-            try:
-                self.engine.execute(f"INSERT INTO telegram_loger.download_audict(file_nm, channel_id, message_id, date,status) VALUES('{file_nm}', '{chat_id}', {msg_id}, '{date}','{status}');")
+            print(traceback.format_exc())
+    def _is_exist(self,chat_id:str,msg_id:int) -> bool:
+        
+        try:
+            data=self.engine.execute(f"select distinct message_id,channel_id from telegram_loger.download_audict where message_id ={msg_id} and channel_id='{chat_id}' and status <> 'Fail' ;").fetchall()
+            print(data)
+            if data:
                 return True
-            except Exception as e:
-                print(e)
+            else:
                 return False
                 
-            finally:
-                self.engine.dispose()
+        except Exception as e:
+            print(traceback.format_exc())
+            return False
+            
+        finally:
+            self.engine.dispose()
+    def update_audict(self,chat_id:str,msg_id:int,date:str,file_nm:str,status:str) -> bool:
+        try:
+            self.engine.execute(f"INSERT INTO telegram_loger.download_audict(file_nm, channel_id, message_id, date,status) VALUES('{file_nm}', '{chat_id}', {msg_id}, '{date}','{status}');")
+            return True
+        except Exception as e:
+            print(traceback.format_exc())
+            return False
+            
+        finally:
+            self.engine.dispose()
+
+        
 
 if __name__ =='__main__':
-    
+
     executer=Media_downloader()
-    executer.main()
+    asyncio.run(executer.main())
     
 
 
--- utils.py
+++ utils.py
@@ -1,2 +1,11 @@
 from  sqlalchemy import create_engine
 from  rich.logging  import RichHandler
+import base64
+
+def decode_pass(encode:str) -> str:
+    sample_string_bytes = encode.encode("ascii")
+  
+    base64_bytes = base64.b64decode(sample_string_bytes)
+    base64_string = base64_bytes.decode("ascii")
+    return base64_string
+
