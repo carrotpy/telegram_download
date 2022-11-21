b3528e3a7b5e7da79ca58c606d326fcf6a6a5f49
Dhilip <dhilidhiva@gmail.com>
2022-11-22 01:13:36 +0530

    7th day adding setup

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ import yaml
 import logging
 from rich.logging  import RichHandler
 import os
-
+import subprocess as sp
+from  sqlalchemy import create_engine
 
 
 config=yaml.load(open('config.yaml'),Loader=yaml.FullLoader)
@@ -26,6 +27,24 @@ log=logging.getLogger('setup_log')
 def main():
     #run setup checks one by one if its precedder is succesfullly executed
     #else stop there and put false in yaml "SETUP_STATUS"
+    sp.Popen('pip3 install  -r requirements.txt', shell=True,close_fds=True)
+    
+    engine=create_engine("postgresql://postgres:Carrotpy123@localhost/postgres")
+    try:
+        engine.execute('CREATE SCHEMA telegram_loger AUTHORIZATION postgres;')
+        engine.execute('''CREATE TABLE telegram_loger.download_audict (
+        file_nm varchar NOT NULL,
+        channel_id varchar NOT NULL,
+        message_id int8 NOT NULL,
+        "date" date NULL,
+        status varchar NULL,
+        channel_nm varchar NULL,
+        CONSTRAINT download_audict_pkey PRIMARY KEY (channel_id, message_id)
+    );''')
+    except Exception as e:
+        print({e})
+    
+        
     pass
 
 
