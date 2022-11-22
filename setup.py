f66fcca7384e8f249259796d1206c1b792fccba6
jai4sankar <jai4sankar@users.noreply.github.com>
2022-11-23 00:03:47 +0530

    setup

--- setup.py
+++ setup.py
@@ -3,34 +3,26 @@
 #make the download path for each channel
 #confirm if all the requirements are fullfilled if not dont run the code
 
-import yaml
-import logging
-from rich.logging  import RichHandler
-import os
+
 import subprocess as sp
-from  sqlalchemy import create_engine
-
-
-config=yaml.load(open('config.yaml'),Loader=yaml.FullLoader)
 
 
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(message)s",
-    datefmt="[%X]",
-    handlers=[RichHandler()],
-)
-
-log=logging.getLogger('setup_log')
 
 def main():
     #run setup checks one by one if its precedder is succesfullly executed
     #else stop there and put false in yaml "SETUP_STATUS"
-    sp.Popen('pip3 install  -r requirements.txt', shell=True,close_fds=True)
+    try:
+        sp.check_call('pip3 install  -r requirements.txt', shell=True,close_fds=True)
+    except Exception as e:
+        print(e)
+    # sp.(args)
     
+
+    from  sqlalchemy import create_engine
     engine=create_engine("postgresql://postgres:Carrotpy123@localhost/postgres")
     try:
+
         engine.execute('CREATE SCHEMA telegram_loger AUTHORIZATION postgres;')
         engine.execute('''CREATE TABLE telegram_loger.download_audict (
         file_nm varchar NOT NULL,
@@ -42,7 +34,7 @@ def main():
         CONSTRAINT download_audict_pkey PRIMARY KEY (channel_id, message_id)
     );''')
     except Exception as e:
-        logging.info({e})
+        print(e)
     finally:
         engine.dispose()
     
